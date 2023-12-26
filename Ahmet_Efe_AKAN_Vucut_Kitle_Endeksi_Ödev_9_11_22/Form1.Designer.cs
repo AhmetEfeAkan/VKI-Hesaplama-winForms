4369e285f06b1d28a7988ca7c1e592cb61ef4653
Ahmet Efe Akan <83471324+AhmetEfeAkan@users.noreply.github.com>
2023-12-26 10:14:04 +0300

    Add files via upload

---
+++ Ahmet_Efe_AKAN_Vucut_Kitle_Endeksi_Ödev_9_11_22/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Vucut_Kitle_Endeksi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelDRM = new System.Windows.Forms.Label();
+            this.labelVKI = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.mstextBox2 = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 47);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(76, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kilonuz (kg):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label2.Location = new System.Drawing.Point(76, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Boyunuz (m):";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.button1.Location = new System.Drawing.Point(67, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Vücut - Kütle İndeksi Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(579, 208);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.labelDRM);
+            this.groupBox1.Controls.Add(this.labelVKI);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(362, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 178);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vücut-Kütle İndeksi Sonuçlar";
+            // 
+            // labelDRM
+            // 
+            this.labelDRM.AutoSize = true;
+            this.labelDRM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.labelDRM.ForeColor = System.Drawing.Color.White;
+            this.labelDRM.Location = new System.Drawing.Point(66, 67);
+            this.labelDRM.Name = "labelDRM";
+            this.labelDRM.Size = new System.Drawing.Size(102, 20);
+            this.labelDRM.TabIndex = 3;
+            this.labelDRM.Text = "Belirlenmedi";
+            // 
+            // labelVKI
+            // 
+            this.labelVKI.AutoSize = true;
+            this.labelVKI.BackColor = System.Drawing.Color.Transparent;
+            this.labelVKI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.labelVKI.ForeColor = System.Drawing.Color.White;
+            this.labelVKI.Location = new System.Drawing.Point(157, 28);
+            this.labelVKI.Name = "labelVKI";
+            this.labelVKI.Size = new System.Drawing.Size(18, 20);
+            this.labelVKI.TabIndex = 2;
+            this.labelVKI.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Durum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Vücut Kitle İndeksi (VKİ) :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(751, 251);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Ahmet Efe AKAN";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(751, 276);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "2022©";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(728, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(19, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "__";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(12, 209);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(551, 134);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Vücut-Kitle İndeksi Bilgilendirme";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 42);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(285, 52);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "18 ile < 25 aralığında ise ‘Normal’\r\n25 ile < 30 aralığında ise ‘Kilolu’\r\n30 ve d" +
+    "aha yüksek ise ‘Obez’\r\n35 ve daha fazla ise ‘Ciddi Obez’ olarak kabul edilmekted" +
+    "ir.\r\n";
+            // 
+            // mstextBox2
+            // 
+            this.mstextBox2.Location = new System.Drawing.Point(201, 81);
+            this.mstextBox2.Mask = "0.00";
+            this.mstextBox2.Name = "mstextBox2";
+            this.mstextBox2.Size = new System.Drawing.Size(100, 20);
+            this.mstextBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(868, 355);
+            this.Controls.Add(this.mstextBox2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Vücut-Kütle İndeksi Hesaplama Programı";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelDRM;
+        private System.Windows.Forms.Label labelVKI;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MaskedTextBox mstextBox2;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Ahmet_Efe_AKAN_Vucut_Kitle_Endeksi_Ödev_9_11_22/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vucut_Kitle_Endeksi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float agirlik, boy, VKİ;
+                agirlik = Convert.ToSingle(textBox1.Text);
+                boy = Convert.ToSingle(mstextBox2.Text);
+                VKİ = agirlik / (boy * boy);
+                labelVKI.Text = VKİ.ToString();
+                if (VKİ < 18.5 )
+                {
+                    labelDRM.Text = "Zayıf";
+                    labelVKI.ForeColor = Color.Red;
+                    labelDRM.ForeColor = Color.Red;
+                }
+                else if (VKİ >= 18.5 && VKİ < 24.9)
+                {
+                    labelDRM.Text = "Normal";
+                    labelVKI.ForeColor = Color.Lime;
+                    labelDRM.ForeColor = Color.Lime;
+                }
+                else if (VKİ >= 24.9 && VKİ < 29.9)
+                {
+                    labelDRM.Text = "Kilolu";
+                    labelVKI.ForeColor = Color.Orange;
+                    labelDRM.ForeColor = Color.Orange;
+                }
+                else if (VKİ >= 29.9 && VKİ < 35)
+                {
+                    labelDRM.Text = "Obez";
+                    labelVKI.ForeColor = Color.Red;
+                    labelDRM.ForeColor = Color.Red;
+                }
+                else if (VKİ >= 35)
+                {
+                    labelDRM.Text = "Ciddi Obez";
+                    labelVKI.ForeColor = Color.DarkRed;
+                    labelDRM.ForeColor = Color.DarkRed;
+                }
+                else if(agirlik<=0&&boy<=0)
+                {
+                    labelVKI.Text = "Hatalı Giriş";
+                    labelDRM.Text = "Hatalı Giriş";
+                    labelVKI.ForeColor = Color.White;
+                    labelDRM.ForeColor = Color.White;
+                    MessageBox.Show("Lütfen Girdiğiniz Kütle/Boy Değerlerini\nKontrol Ediniz.", "Vücut Kütle İndeksi");
+                }
+                
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen Girdiğiniz Kütle/Boy Değerlerini\nKontrol Ediniz.", "Vücut Kütle İndeksi");
+            }
+    }
+    }
+}
